8c4e6179cd882f75f791e7d40001ea6d75ad79d6
[Celina Hansen] <[ceha19ab@student.cbs.dk]>
2019-10-09 14:02:58 +0200

    niende commit

---
+++ Classes/JoinUser.js
@@ -0,0 +1,14 @@
+class JoinUser extends User {
+    constructor(username, Categories, pastJoinedEvents); {
+        super(username);
+        this._Categories = Categories;
+        this._pastJoinedEvents = pastJoinedEvents;
+
+}
+    participateEvent() {
+
+}
+    leaveEvent() {
+
+}
+}
