7cca1932667765fc2ec34f9464f63147e47654f5
emilhertz <emhe19ae@student.cbs.dk>
2019-10-14 13:22:27 +0200

    login 2

--- Functions/login.js
+++ Functions/login.js
@@ -16,8 +16,23 @@ var user = [
     ];
 
 //for-loop, der går gennem user-array og tjekker om username eksisterer, og om password stemmer overens.
-function tjekLogin() {
+var submitButton = document.getElementById("submit")
+submitButton.onclick = function (e) {
+    //Funktion der forhindrer opdatering af konsol
+    e.preventDefault()
+
     //Henter information fra login-side
     var username = document.getElementById("loginUsername");
-    var password = document.getElementById()
+    var password = document.getElementById("loginPassword");
+
+    for (i=0; i<user.length; i++){
+        if (username.value === user[i].username && password.value === user[i].password){
+            console.log(username.value + " er logget ind!")
+            return
+        }
+    }
+    console.log("Forkert brugernavn eller password :(")
+
 }
+
+//Lav funktion der kan tilføje og slette brugere (push-pop nested array)
