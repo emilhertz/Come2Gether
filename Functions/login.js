415473984ac33fff32063915b099d69c235c52d8
emilhertz <emhe19ae@student.cbs.dk>
2019-10-14 12:18:19 +0200

    login commit

--- Functions/login.js
+++ Functions/login.js
@@ -1,10 +1,23 @@
-//Hent username og password fra html-form
-this.username = document.getElementById("username");
-this.password = document.getElementById("password");
+//Denne  funktion er ikke færdig, men virker i praksis
+//user array(nested) - database der indeholder username og password
+var user = [
+    {
+        username: "peter",
+        password: "kanin"
+    },
+    {
+        username: "thomas",
+        password: "tog"
+    },
+    {
+        username: "jens",
+        password: "ole"
+    }
+    ];
 
-//Gem input fra html-form i localstorage
-function gemLogin() {
-    //
-    localStorage.setItem("username", username.value);
-    localStorage.setItem("password", password.value);
+//for-loop, der går gennem user-array og tjekker om username eksisterer, og om password stemmer overens.
+function tjekLogin() {
+    //Henter information fra login-side
+    var username = document.getElementById("loginUsername");
+    var password = document.getElementById()
 }
