37d3c69f0b7d0f1153f963c31165272da69af3ec
[Celina Hansen] <[ceha19ab@student.cbs.dk]>
2019-10-18 09:37:27 +0200

    join user

--- Classes/JoinUser.js
+++ Classes/JoinUser.js
@@ -3,12 +3,26 @@ class JoinUser extends SuperUser {
         super(username);
         this._Categories = Categories;
         this._pastJoinedEvents = pastJoinedEvents;
-
 }
-    participateEvent() {
 
-}
-    leaveEvent() {
+    present () {
+        return "Du har deltaget i " + this._pastJoinedEvents
+    }
 
+
+
+
+participateEvent = function () {
+
+    
 }
+    function participateEvent(eventName) {
+        this._eventName = eventName;
+            return "Du har tilmeldt dig " + this._eventName;
+        }
+
+    function leaveEvent(leaveEvent) {
+        this._leaveEvent = leaveEvent;
+        return "Du er nu frameldt " + this._leaveEvent;
+        }
 }
