e328ad107a7e50d69f22e5c65ebc8a6c88caafe8
emilhertz <emhe19ae@student.cbs.dk>
2019-10-18 09:55:09 +0200

    Ny Users klasse

--- Classes/HostUser.js
+++
@@ -1,9 +0,0 @@
-class HostUser extends SuperUser {
-    costructor(username, pastHostedEvents) {
-        super(username);
-        this._pastHostedEvents = pastHostedEvents;
-    }
-    get pastHostedEvents() {
-        return this._pastHostedEvents;
-    }
-}
