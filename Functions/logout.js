6318f61d79f402450535542bda50d3cc721f051d
emilhertz <emhe19ae@student.cbs.dk>
2019-10-18 12:37:18 +0200

    Login-funktion

--- Functions/logout.js
+++ Functions/logout.js
@@ -4,3 +4,13 @@ function logout() {
     localStorage.setItem("login_status", "false");
     window.open("../HTML/home.html", "_self")
 }
+
+// Skjul login-knap
+if (localStorage.getItem("login_status") === "true") {
+    document.getElementById("login_button").style.visibility = "hidden";
+}
+
+// Skjul logout-knap
+if (localStorage.getItem("login_status") !== "true") {
+    document.getElementById("logout_button").style.visibility = "hidden";
+}
