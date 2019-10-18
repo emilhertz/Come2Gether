b6a850f7f14f5bdea6c3af1dcd51c4707c966ed8
emilhertz <emhe19ae@student.cbs.dk>
2019-10-18 10:41:54 +0200

    Login-funktion meddelelse

---
+++ Classes/Users.js
@@ -0,0 +1,22 @@
+// User-klasse
+class User {
+    constructor(username, password, age, location, pastJoinedEvents, pastHostedEvents) {
+        this.username = username;
+        this.password = password;
+        this.age = age;
+        this.location = location;
+        this.pastJoinedEvents = pastJoinedEvents;
+        this.pastHostedEvents = pastHostedEvents;
+    }
+}
+
+// Array der bruges til login-process
+let listOfUsers = [];
+
+// Hardcodede værdier til array
+listOfUsers.push(new User("Thorn", "Password", 32, "København","Ingen", "Ingen"));
+listOfUsers.push(new User("Thomas", "Tog", 90, "Togskinnerne", "Ingen", "Ingen"));
+listOfUsers.push(new User("Peter", "Kanin", 300, "Hareskoven", "Ingen", "Ingen"));
+
+// Liste af login i konsol
+console.log(listOfUsers);
--- Functions/login.js
+++ Functions/login.js
@@ -1,6 +1,4 @@
-//Denne funktion henter listOfSuperUsers-array fra SuperUser.js
-
-//Login-funktion, der aktiveres når submit-knap med id="login" trykkes
+//Denne funktion henter listOfSuperUsers-array fra User.jsogin-funktion, der aktiveres når submit-knap med id="login" trykkes
 var loginButton = document.getElementById("login");
 loginButton.onclick = function (e) {
     //preventDefault forhindrer opdatering af konsol
@@ -11,11 +9,15 @@ loginButton.onclick = function (e) {
     var password = document.getElementById("loginPassword");
 
     //for-loop der tjekker om indtastede værdier stemmer overens med array
-    for (i=0; i<listOfSuperUsers.length; i++){
-        if (username.value === listOfSuperUsers[i].username && password.value === listOfSuperUsers[i].password){
-            console.log(username.value + " er logget ind!");
+    for (i=0; i<listOfUsers.length; i++){
+        if (username.value === listOfUsers[i].username && password.value === listOfUsers[i].password){
+            //  Gem username i localstorage
+            localStorage.setItem("username", username.value);
+            // (i fremtiden) Sæt login_status til true
+            // Åbner home.html
+            window.open("../HTML/home.html", "_self")
             return
         }
     }
-    console.log("Forkert brugernavn eller password :(")
+    alert("Forkert brugernavn eller password :(")
 };
