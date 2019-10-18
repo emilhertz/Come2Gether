c860c1844bb3d1a6fc7a4464465269d3ae27e515
emilhertz <emhe19ae@student.cbs.dk>
2019-10-18 09:19:04 +0200

    Login-funktion færdig

--- Classes/SuperUser.js
+++ Classes/SuperUser.js
@@ -1,6 +1,17 @@
 class SuperUser {
     constructor(username, password) {
-        this._username = username;
-        this._password = password;
+        this.username = username;
+        this.password = password;
     }
 }
+
+// Array der bruges til login-process
+let listOfSuperUsers = [];
+
+// Hardcodede værdier til array
+listOfSuperUsers.push(new SuperUser("Thorn", "password"));
+listOfSuperUsers.push(new SuperUser("Thomas", "Tog"));
+listOfSuperUsers.push(new SuperUser("Peter", "Kanin"));
+
+// Liste af login i konsol
+console.log(listOfSuperUsers);
