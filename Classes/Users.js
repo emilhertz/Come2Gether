c70829064fefacdb00a16732a023984fec4eda6e
emilhertz <emhe19ae@student.cbs.dk>
2019-10-20 21:17:07 +0200

    Opret bruger funktionalitet

--- Classes/Users.js
+++ Classes/Users.js
@@ -9,14 +9,3 @@ class Users {
         this.pastHostedEvents = pastHostedEvents;
     }
 }
-
-// Array der bruges til login-process
-//let listOfUsers = [];
-
-// Hardcodede værdier til array
-//listOfUsers.push(new User("Thorn", "Password", 32, "København","Ingen", "Ingen"));
-//listOfUsers.push(new User("Thomas", "Tog", 90, "Togskinnerne", "Ingen", "Ingen"));
-//listOfUsers.push(new User("Peter", "Kanin", 300, "Hareskoven", "Ingen", "Ingen"));
-
-// Liste af login i konsol
-//console.log(listOfUsers);
--- Functions/createUser.js
+++ Functions/createUser.js
@@ -1,6 +1,12 @@
-var createButton = document.getElementById("opretBruger");
-createButton.onclick = function (e) {
-    //preventDefault forhindrer opdatering af konsol
-    e.preventDefault();
+//Opret bruger funktion, som så skal gemmes i localStorage sammen med eksisterende brugere
+function createUser() {
+    //Gemmer informationer fra ny bruger i objekt newUser
+    newUser = new Users("document.getElementById('username')","doc");
 
-    // Opret bruger funktion, som så skal gemmes i localStorage sammen med eksisterende brugere
+    //Henter storedListOfUsers i localStorage og "parser" til array, hvor newUsers "pushes"
+    var listOfUsers = JSON.parse(localStorage.getItem("storedListOfUsers"));
+    listOfUsers.push(newUser);
+    //listOfUsers stringifies og sættes som den nye værdi i localStorage
+    var listOfUsersString = JSON.stringify(listOfUsers);
+    localStorage.setItem("storedListOfUsers", listOfUsersString);
+}
