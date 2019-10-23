ac5504a6f0515dd648d0b028facfca95b1bf1bdb
emilhertz <emhe19ae@student.cbs.dk>
2019-10-23 13:41:17 +0200

    Færdig logind og logud

---
+++ Functions/dummyUsers.js
@@ -0,0 +1,12 @@
+// If-statement der ser om localstorage med key: listOfUsers er tom. Hvis det er tilfældet bliver hard-codede brugere tilknyttet som en string
+if (localStorage.getItem("storedListOfUsers") == null) {
+    var listOfUsers = [];
+    listOfUsers.push(new Users("Thorn","password","32","København","Fodbold for nybegyndere","Vinsmagning"));
+    listOfUsers.push(new Users("Peter","Kanin","224","Eventyrskoven","Kaninfodring","Kaninræs"));
+
+    var listOfUsersString = JSON.stringify(listOfUsers);
+    localStorage.setItem("storedListOfUsers", listOfUsersString);
+} else {
+    // localStorage med key: "storedListOfUsers" er ikke tomt, og den data hentes og gøres til objekter i array'et listOfUsers
+    var listOfUsers = JSON.parse(localStorage.getItem("storedListOfUsers"));
+}
