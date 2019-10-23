2dbb8ec0ef5e1dfcb03f618ef614c8305487abf5
emilhertz <emhe19ae@student.cbs.dk>
2019-10-23 15:03:19 +0200

    Eventomdøbning

--- Functions/Events/createEvent.js
+++ Functions/Events/createEvent.js
@@ -6,13 +6,16 @@ createUserButton.onclick = function (e) {
     //Opret event funktion, som gemmer nyt event i localStorage sammen med eksisterende event (push)
 
     //Gemmer informationer fra nyt event-form i variabler
-    var newUsername = document.getElementById("username").value;
-    var newPassword = document.getElementById("password").value;
-    var newAge = document.getElementById("age").value;
-    var newLocation = document.getElementById("lokation").value;
+    // var newUsername = document.getElementById("username").value;
+    // var newPassword = document.getElementById("password").value;
+    // var newAge = document.getElementById("age").value;
+    // var newLocation = document.getElementById("lokation").value;
 
+
+    //Events
     //Henter storedListOfEvents i localStorage og "parser" til array, hvor nyt event "pushes"
-    listOfEvents.push(new Events());
+    var listOfEvents = JSON.parse(localStorage.getItem("storedListOfEvents"));
+    listOfEvents.push(new Events("test"));
     //listOfUsers stringifies og overskriver storedListOfUsers localStorage
     var listOfEventsString = JSON.stringify(listOfEvents);
     localStorage.setItem("storedListOfUsers", listOfEventsString);
