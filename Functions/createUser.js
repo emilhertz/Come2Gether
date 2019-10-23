b0fd3f6ab0c352a0ea9f1681520a0279c836777f
emilhertz <emhe19ae@student.cbs.dk>
2019-10-23 12:30:51 +0200

    Opret bruger funktionalit

--- Functions/createUser.js
+++ Functions/createUser.js
@@ -1,12 +1,14 @@
-//Opret bruger funktion, som så skal gemmes i localStorage sammen med eksisterende brugere
+//Opret bruger funktion, som gemmer ny bruger i localStorage sammen med eksisterende brugere (push)
 function createUser() {
     //Gemmer informationer fra ny bruger i objekt newUser
-    var newUsername = document.getElementById("username");
-    newUser = new Users("newUsername","doc");
+    var newUsername = document.getElementById("username").value;
+    // var newPassword = document.getElementById("password");
+    // newUser = new Users("newUsername", 'newPassword',"32", "København","","");
 
+    localStorage.setItem("usernameTest", newUsername);
     //Henter storedListOfUsers i localStorage og "parser" til array, hvor newUsers "pushes"
     var listOfUsers = JSON.parse(localStorage.getItem("storedListOfUsers"));
-    listOfUsers.push(newUser);
+    listOfUsers.push(new Users(newUsername));
     //listOfUsers stringifies og sættes som den nye værdi i localStorage
     var listOfUsersString = JSON.stringify(listOfUsers);
     localStorage.setItem("storedListOfUsers", listOfUsersString);
