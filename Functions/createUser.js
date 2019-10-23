22d3915f2ba6826ebaed39c4165d1b6ea3506858
emilhertz <emhe19ae@student.cbs.dk>
2019-10-23 12:48:47 +0200

    Opret bruger funktionalit

--- Functions/createUser.js
+++ Functions/createUser.js
@@ -1,15 +1,26 @@
 //Opret bruger funktion, som gemmer ny bruger i localStorage sammen med eksisterende brugere (push)
 function createUser() {
-    //Gemmer informationer fra ny bruger i objekt newUser
+    //Gemmer informationer fra ny bruger-form i variabler
     var newUsername = document.getElementById("username").value;
-    // var newPassword = document.getElementById("password");
-    // newUser = new Users("newUsername", 'newPassword',"32", "København","","");
+    var newPassword = document.getElementById("password").value;
+    var newAge = document.getElementById("age").value;
+    var newLocation = document.getElementById("lokation").value;
 
-    localStorage.setItem("usernameTest", newUsername);
-    //Henter storedListOfUsers i localStorage og "parser" til array, hvor newUsers "pushes"
+    //Henter storedListOfUsers i localStorage og "parser" til array, hvor ny bruger "pushes"
     var listOfUsers = JSON.parse(localStorage.getItem("storedListOfUsers"));
-    listOfUsers.push(new Users(newUsername));
+    listOfUsers.push(new Users(newUsername, newPassword, newAge, newLocation, "", ""));
     //listOfUsers stringifies og sættes som den nye værdi i localStorage
     var listOfUsersString = JSON.stringify(listOfUsers);
     localStorage.setItem("storedListOfUsers", listOfUsersString);
+    //alert bruger oprettet
+    //retur til home-page
+    //sørg for at storedListOfUsers altid er oprettet
 }
+
+
+
+//Validering af form (Celina)
+
+//Tjek at alder er mellem 13 og 99 (alert)
+//Tjek at password minimum er 6 tegn (alert)
+//Tjek at brugernavn ikke eksisterer i database (alert)
