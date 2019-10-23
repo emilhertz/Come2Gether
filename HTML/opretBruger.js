d310e1aa14b75e8687070cd4378ec7a08d67942d
[Celina Hansen] <[ceha19ab@student.cbs.dk]>
2019-10-23 12:30:45 +0200

    js function

---
+++ HTML/opretBruger.js
@@ -0,0 +1,10 @@
+function rigthInput () {
+    var name = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var age = document.getElementById("age").value;
+    var location = document.getElementById("lokation").value;
+
+
+    var form_valid = true;
+    var validation_message = "";
+}
