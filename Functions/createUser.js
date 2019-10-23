591d2c99af0bd34bc2d461bb8e3e17532b1d1f7f
emilhertz <emhe19ae@student.cbs.dk>
2019-10-23 13:09:28 +0200

    Færdig opretbruger funktion

--- Functions/createUser.js
+++ Functions/createUser.js
@@ -9,18 +9,21 @@ function createUser() {
     //Henter storedListOfUsers i localStorage og "parser" til array, hvor ny bruger "pushes"
     var listOfUsers = JSON.parse(localStorage.getItem("storedListOfUsers"));
     listOfUsers.push(new Users(newUsername, newPassword, newAge, newLocation, "", ""));
-    //listOfUsers stringifies og sættes som den nye værdi i localStorage
+    //listOfUsers stringifies og overskriver storedListOfUsers localStorage
     var listOfUsersString = JSON.stringify(listOfUsers);
     localStorage.setItem("storedListOfUsers", listOfUsersString);
-    //alert bruger oprettet
-    //retur til home-page
-    //sørg for at storedListOfUsers altid er oprettet
+    //Giver besked om ny bruger er oprettet
+    alert(newUsername + " er nu oprette som bruger!");
+    //Åbner home.html når bruger er oprettet (virker ikke)
+    window.open("../HTML/home.html", "_self");
 }
 
 
 
 //Validering af form (Celina)
 
+//Tjek at der overhoved er indtastet værdier (alert)
+//Tjek at by er valgt i drop-down (og ikke bare byer)
 //Tjek at alder er mellem 13 og 99 (alert)
 //Tjek at password minimum er 6 tegn (alert)
 //Tjek at brugernavn ikke eksisterer i database (alert)
