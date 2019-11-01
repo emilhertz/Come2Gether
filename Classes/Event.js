5978a2a21230a9392ac1895e66497eb7d2189201
emilhertz <emhe19ae@student.cbs.dk>
2019-11-01 21:38:24 +0100

    Arbejde på opret event funktionalitet

--- Classes/Event.js
+++ Classes/Event.js
@@ -1,10 +1,12 @@
 class Events {
-    constructor(eventName, eventLocation, Category, eventTime, eventDescription) {
+    constructor(eventName, eventLocation, Category, eventTime, eventDescription, eventCapacity, eventHost) {
         this.eventName = eventName;
         this.eventLocation = eventLocation;
         this.Category = Category;
         this.eventTime = eventTime;
         this.eventDescription = eventDescription;
+        this.eventCapacity = eventCapacity;
+        this.eventHost = eventHost;
     }
 }
 
--- Functions/Events/createEvent.js
+++ Functions/Events/createEvent.js
@@ -6,13 +6,11 @@ createUserButton.onclick = function (e) {
 
     //Gemmer informationer fra nyt event-form i variabler
     var eventName = document.getElementById("eventName").value;
-    // var eventLocation = document.getElementById("eventLocation");
 
-    //event category
+    //Henter eventCategory og ser hvilken kategori er valgt med for-loop
     var categoryButtons = document.getElementsByName("categories");
     var eventCategory = "";
 
-    //for loop that choses the radio button that is checked
     for (i=0; i<categoryButtons.length; i++){
         if (categoryButtons[i].checked) {
             eventCategory = categoryButtons[i].value;
@@ -20,12 +18,26 @@ createUserButton.onclick = function (e) {
         }
     }
 
+    //Indtil der findes en løsning med adresser, bruges kun byer
+    // var eventStreet = document.getElementById("gadenavn");
+    // var eventHouseNo = document.getElementById("husnummer");
+    // var eventLocation = eventStreet + " " + eventHouseNo + ", " + eventCity;
+    var eventCity = document.getElementById("city");
+
+
+
     var eventTime = document.getElementById("time").value;
     var eventDescription = document.getElementById("eventDescription").value;
 
+    //Validering (mangler - se createUser)
+
+
+    //Henter bruger som er logget ind, så denne tilknyttes event som hostUser
+    var hostUser = JSON.parse(localStorage.getItem("signedIn"));
+
     //Henter storedListOfEvents i localStorage og "parser" til array, hvor nyt event "pushes"
     var listOfEvents = JSON.parse(localStorage.getItem("storedListOfEvents"));
-    listOfEvents.push(new Events(eventName, "x", eventCategory, eventTime, eventDescription));
+    listOfEvents.push(new Events(eventName, eventCity, eventCategory, eventTime, eventDescription, "et tal", hostUser.username));
     //listOfEvents stringifies og overskriver storedListOfEvents i localStorage
     var listOfEventsString = JSON.stringify(listOfEvents);
     localStorage.setItem("storedListOfEvents", listOfEventsString);
